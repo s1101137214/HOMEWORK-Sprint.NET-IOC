e2c7a9a7a07ea5ba9b72161cccdd1185931ce6cf
asd890303 <asd890303@gmail.com>
2015-04-10 18:14:34 +0800

    first

---
+++ KuasCore/Dao/Impl/NewemployeeDao.cs
@@ -0,0 +1,46 @@
+﻿using KuasCore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KuasCore.Dao.Impl
+{
+    class NewemployeeDao : IEmployeeDao
+    {
+
+        public IList<Employee> GetAllEmployees()
+        {
+            List<Employee> companies = new List<Employee>();
+
+            Employee Employee1 = new Employee();
+            Employee1.Id = "haha";
+            Employee1.Name = "叡揚資訊!!";
+            Employee1.Age = 7;
+            companies.Add(Employee1);
+
+            Employee Employee2 = new Employee();
+            Employee2.Id = "KUAS";
+            Employee2.Name = "高雄應用科技大學";
+            companies.Add(Employee2);
+
+            return companies;
+        }
+
+        public Employee GetEmployeeById(string id)
+        {
+
+            Employee Employee = null;
+
+            if ("haha".Equals(id))
+            {
+                Employee = new Employee();
+                Employee.Id = "haha";
+                Employee.Name = "叡揚資訊!!!!";
+            }
+
+            return Employee;
+        }
+    }
+}
